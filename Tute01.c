a7b6a509e7e1af0207daa9436587ce7ab70c7bbc
Naveen  Fonseka <87698329+IT21010262@users.noreply.github.com>
2021-08-01 11:27:37 +0000

    Done

--- Tute01.c
+++ Tute01.c
@@ -3,9 +3,21 @@
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
 #include <stdio.h>
+int main() 
+{
+  int mark1,mark2;
+  float sum,avg;
 
-int main() {
-  
+  printf("Enter the First  Mark :");
+  scanf("%d",&mark1);
+  printf("Enter the Second  Mark :");
+  scanf("%d",&mark2);
+
+  sum=mark1+mark2;
+  avg=sum/2;
+
+  printf("Avarage is : %.2f",avg);
+
   return 0;
 }
 
